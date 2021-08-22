732a8baaf9ee81b6d018f700fb07265eac522cdc
kraiviks <kraiviks@gmail.com>
2021-08-23 02:01:19 +0300

    v0.3

---
+++ src/components/app/app.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import Title from "../title/title";
+import "./app.css";
+import {
+	Container,
+	Card,
+	CardTitle,
+	CardText,
+	CardImg,
+	CardImgOverlay,
+	Input,
+	InputGroup,
+	InputGroupAddon,
+	InputGroupText,
+} from "reactstrap";
+import axios from "axios";
+
+const App = () => {
+	const [data, setData] = useState([]);
+	useEffect((v) => getApi("Hello"), []);
+	const getApi = async (film) => {
+		return await axios(
+			`https://www.omdbapi.com/?s=${film}&apikey=d869b50f`
+		).then((resp) => setData(resp.data.Search));
+	};
+
+	const validData = data ? data : [];
+
+	const filmsArr = validData.map((item, i) => {
+		return (
+			<Card inverse key={i}>
+				<CardImg
+					width="100%"
+					src={
+						item.Poster !== "N/A"
+							? item.Poster
+							: process.env.PUBLIC_URL + "/Noimage.svg"
+					}
+					alt="Card image cap"
+				/>
+				<CardImgOverlay>
+					<CardTitle tag="h5">{item.Title}</CardTitle>
+				</CardImgOverlay>
+				<CardText>{item.Type}</CardText>
+				<CardText>{item.Year}</CardText>
+			</Card>
+		);
+	});
+
+	return (
+		<div className="app">
+			<Container>
+				<Title />
+				<InputGroup>
+					<InputGroupAddon addonType="prepend">
+						<InputGroupText>Enter film name</InputGroupText>
+					</InputGroupAddon>
+					<Input
+						placeholder="film"
+						onChange={(e) => getApi(e.target.value)}
+					/>
+				</InputGroup>
+				<div className="card-box">{filmsArr}</div>
+			</Container>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/components/title/title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './title.css'
+const Title = () => {
+	return (
+		<h1 className="title mb-5">
+			Movies DB (Search)	
+		</h1>
+	)
+}
+
+export default Title
